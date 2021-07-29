7233d90dfb2b40c832afb04a159718817a32d3ce
alexbaekey <abaekey@knights.ucf.edu>
2021-07-29 10:17:57 -0400

    Added basic graph

--- dash_graphs.py
+++ dash_graphs.py
@@ -5,7 +5,24 @@ import dash_html_components as html
 app=dash.Dash()
 
 #starts a div tag
-app.layout = html.Div('Dash tutorials') 
+app.layout = html.Div(children=[
+    html.H1('Dash tutorials'),
+    dcc.Graph(id='example',
+        figure={
+            'data': [
+                {'x':[1,2,3,4,5,6,7], 'y':[100,101,104,107,91,108,101], 'type':'line', 'name':'boats'},
+                {'x':[1,2,3,4,5,6,7], 'y':[100,101,104,107,91,108,101], 'type':'bar', 'name':'cars'},
+                ],
+            'layout': {
+                'title':'Basic Dash Example'
+                }
+            })
+
+    ]) 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+
