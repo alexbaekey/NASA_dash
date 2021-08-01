6de8e7166de387f702a6d01c9603e6074cf3d907
Alexander Baekey <abaekey@knights.ucf.edu>
2021-08-01 14:42:39 -0400

    improved functionality with slider, but still glitchy

--- NASA_dash2.py
+++ NASA_dash2.py
@@ -1,5 +1,3 @@
-#https://plotly.com/python/sliders/
-
 import plotly.graph_objects as go # or plotly.express as px
 from PIL import Image
 import dash
@@ -18,7 +16,13 @@ from sklearn import linear_model
 # (2.5,1.5) = Cambridge
 # (1.5,2.5) = Annapolis
 
+# coastal heights
+h_annapolis = 3.0
+h_somewhere = 7.0
+
+
 #load up csv from NOAA, not working ...
+#TODO set up S3 for csv, local storage on heroku is difficult?
 #url = "http://tidesandcurrents.noaa.gov/sltrends/data/8575512_meantrend.csv"
 #response = requests.post(url,data={'':})
 #df_anna_sea = pd.read_csv(io.StringIO(response.decode('utf-8')))
@@ -53,8 +57,8 @@ fig3.update_yaxes(title="Historical MSL")
 fig2.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightPink', range=[0,4],tickvals=[0,1,2,3])
 fig2.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightPink', range=[0,4],tickvals=[0,1,2,3])
 
-fig.update_xaxes(title="Year")
-fig.update_yaxes(title="trend")
+fig.update_xaxes(title="Year",range=[1930,3000])
+fig.update_yaxes(title="trend",range=[0,10])
 
 
 df_anna_sea['Year']=df_anna_sea['Year']+df_anna_sea['Month']/12
@@ -73,21 +77,29 @@ y = df_anna_sea['Monthly_MSL'].values.reshape(length,1)
 regr = linear_model.LinearRegression()
 regr.fit(x,y)
 
-
+    
 # add traces per step
-for step in np.arange(1930, 3000, 1):
-    fig.add_trace(
+for step in np.arange(1930, 3000, 50):
+    fig.add_traces(
+        [
+        go.Scatter(
+            x=np.arange(1930, 3000, 50),
+            y=float(regr.coef_)*np.arange(1930, 3000, 50)+float(regr.intercept_),
+        ),
         go.Scatter(
             visible=False,
-            line=dict(color="#00CED1", width=6),
+            line=dict(color="#00CED1", width=3),
             name="year = " + str(1930+step),
-            x=np.arange(1930, step, 1),
-            #y=np.sin(2.22*np.arange(2019,2500,1)+1)))
-            #y=0.00371*np.arange(2019,3000,1)-7.382 # 3.71mm/year mean trend, -7.382m at year 0
-            # using values from tidesandcurrents.noaa.gov for Annapolis, MD 
-            y=float(regr.coef_)*np.arange(1930, 3000, 1)+float(regr.intercept_),
+            x=[step,step],
+            y=[0,10]
         )
+        ]
     )
+
+
+
+fig.add_annotation(text="h_annapolis",xref="paper",yref="paper",x=10,y=h_annapolis,showarrow=False)
+
 
     # Update grid
     #    for i in stations:    
@@ -109,16 +121,23 @@ fig.data[1].visible = True
 # Create and add slider
 steps = []
 for i in range(len(fig.data)):
+    print( " THE NUMBER i " + str(i))
     step = dict(
         method="update",
-        args=[{"visible": [False] * len(fig.data)},
-              {"title": "Projection for year: " + str(1930+i)}],  # layout attribute
+        args=[
+            {"visible": [False] * int(3000-1930/50)},
+            #{"visible": [False]*i},
+              {"title": "Projection for year: " + str(1930+i*50)}],  # layout attribute
     )
     step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
     steps.append(step)
 
+#print("fig data\n")
+#print(fig.data)
+#print(type(fig.data))
+
 sliders = [dict(
-    active=70,
+    active=1,
     currentvalue={"prefix": "Year: "},
     pad={"t": 50},
     steps=steps
@@ -126,7 +145,20 @@ sliders = [dict(
 
 fig.update_layout(
     sliders=sliders,
-    title = "Sea-Level Prediciton in Maryland"
+    title = "Sea-Level Prediciton in Maryland",
+    shapes=[
+        dict(
+            type='line',
+            yref='y', y0= h_annapolis, y1=h_annapolis,
+            xref='paper', x0=0, x1=1
+        ),
+        dict(
+            type='line',
+            yref='y', y0= h_somewhere, y1=h_somewhere,
+            xref='paper', x0=0, x1=1
+        )
+    ]
+
     )
 
 fig2.update_layout(
@@ -138,7 +170,7 @@ fig2.update_layout(
 fig3.update_layout(
     width = 600,
     height = 400,
-    title = "trendline"
+    title = "Annapolis trendline"
     )
 
 app = dash.Dash()
@@ -161,21 +193,7 @@ app.layout = html.Div([
     #style={'width': '48%','display': 'inline-block'}
 )
 
-
-# known to work:
-#app.layout = html.Div([ # dash.plotly.com/layout
-#    dcc.Graph(figure=fig)
-#    dcc.Graph(figure=fig2),
-#    dcc.Graph(figure=fig3),
-#    ],
-#    #style={'columnCount': 2}
-#    #style={'width': '48%','display': 'inline-block'}
-#)
-
-
-
-
-
-
-
 app.run_server(debug=True, use_reloader=True)  # Turn off reloader if inside Jupyter
+
+
+
