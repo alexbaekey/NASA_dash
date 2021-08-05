e27a0d40bee9994f9e2c7f936db25f0fc4d3d48d
Alexander Baekey <abaekey@knights.ucf.edu>
2021-08-04 22:50:33 -0400

    stylistic progress, added some more graphs, added Eunices images

--- NASA_dash2.py
+++ NASA_dash2.py
@@ -10,27 +10,38 @@ import requests
 import io
 from sklearn import linear_model
 
-
-# GRID POSITIONS
-# (1.5,0.5) = Cove Point
-# (2.5,1.5) = Cambridge
-# (1.5,2.5) = Annapolis
+#TODO black background, white text, ariel
+#Make into more of "Regional what-if analysis tool" with dropdowns for scenarios
+#Clean!!
+# layout including sidebar and central plots, side plots on right
 
 # coastal heights
-h_annapolis = 3.0
+h_baltimore = 3.0
 h_somewhere = 7.0
 
 csv_url = 's3://nasa-dash-blue-marble/8575512_meantrend.csv'
-#img_url = 's3://nasa-dash-blue-marble/maryland_coast.jpg'
-img_url = 'https://i.ibb.co/0jTLg38/maryland-coast.jpg'
-# This image link works! Was having so much trouble reading from S3 to image
-# however, website will take down link in a month
-# img = Image.open(img_url)
+#img_url = 'https://i.ibb.co/0jTLg38/maryland-coast.jpg'
+img_url = 'https://i.ibb.co/58169Z1/image001.png'
 
 df_anna_sea = pd.read_csv(csv_url, index_col=False)
 
 fig2 = go.Figure() # or any Plotly Express function e.g. px.bar(...)
 fig3 = go.Figure()
+
+x1=np.random.randn(500)
+fig4 = go.Figure(data=[go.Histogram(x=x1)])
+
+y_0 = np.random.randn(75)-0.5
+y_1 = np.random.randn(75)+0.5
+y_2 = np.random.randn(120)-0.4
+
+fig5 = go.Figure()
+
+fig5.add_trace(go.Box(y=y_0))
+fig5.add_trace(go.Box(y=y_1))
+fig5.add_trace(go.Box(y=y_2))
+
+#maybe this next? https://plotly.com/python/scattermapbox/
 
 
 fig2.add_layout_image(
@@ -40,10 +51,10 @@ fig2.add_layout_image(
             yref="y",
             x=0,
             y=4,
-            sizex=4,
+            sizex=6,
             sizey=4,
             sizing="stretch",
-            opacity=0.5,
+            opacity=0.95,
             layer="below",
             )
         )
@@ -51,7 +62,7 @@ fig2.add_layout_image(
 fig3.update_xaxes(title="Year")
 fig3.update_yaxes(title="Mean Sea Level (MSL)")
 
-fig2.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightPink', range=[0,4],tickvals=[0,1,2,3])
+fig2.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightPink', range=[0,6],tickvals=[0,1,2,3,4,5])
 fig2.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightPink', range=[0,4],tickvals=[0,1,2,3])
 
 
@@ -108,9 +119,9 @@ for step in np.arange(1930,3000,10):
 for step in np.arange(1930,3000,10):
     temp3=go.Scatter(
         x=[1930,1930],
-        y=[h_annapolis,h_somewhere],
+        y=[h_baltimore,h_somewhere],
         mode="markers+text",
-        text=["h_annapolis","h_somewhere"],
+        text=["h_baltimore","h_somewhere"],
         textposition="top right",
         showlegend=False
     )
@@ -174,6 +185,9 @@ fig3.add_trace(
         )
     )
 
+
+
+
 # display inital trace value on timeline
 fig.data[1].visible = True
 
@@ -196,19 +210,22 @@ sliders = [dict(
     currentvalue={"prefix": "Year: "},
     #pad={"t": 50},
     steps=steps,
-    tickcolor='white',
+    tickcolor='black',
     font=dict(
-        color='white'
+        color='black'
         )
 )]
 
 fig.update_layout(
     sliders=sliders,
     title = "Sea-Level Prediciton in Maryland",
+    paper_bgcolor='rgb(0,0,0)',
+    font_color='white',
+    font_family='ariel',
     shapes=[
         dict(
             type='line',
-            yref='y', y0= h_annapolis, y1=h_annapolis,
+            yref='y', y0= h_baltimore, y1= h_baltimore,
             xref='paper', x0=0, x1=1,
 
         ),
@@ -222,21 +239,65 @@ fig.update_layout(
 
 fig2.update_layout(
     width=500,
-    height=500,
-    title= "Underwater? (in progress)"
+    height=400,
+    title= "Baltimore demographics",
+    paper_bgcolor='rgb(0,0,0)',
+    font_color='white',
+    font_family='ariel'
     )
 
 fig3.update_layout(
-    width = 600,
+    width = 500,
     height = 500,
-    title = "Historical MSL data - Annapolis - NOAA"
+    title = "Historical MSL data - Baltimore - NOAA",
+    paper_bgcolor='rgb(0,0,0)',
+    font_color='white',
+    font_family='ariel'
     )
+
+fig4.update_layout(
+    width = 500,
+    height = 500,
+    title = "Bar Charts!",
+    paper_bgcolor='rgb(0,0,0)',
+    font_color='white',
+    font_family='ariel'
+    )
+
+fig5.update_layout(
+    width = 500,
+    height = 500,
+    title = "Box Plots!",
+    paper_bgcolor='rgb(0,0,0)',
+    font_color='white',
+    font_family='ariel'
+    )
+
+
+
+
+
+
+
+#app layout stuff
+img2_url = 'https://i.ibb.co/12p7vTF/bluemarbleheader.png' 
+
 
 app = dash.Dash()
 server=app.server
-app.layout = html.Div([ 
+app.layout = html.Div(children=[ 
     html.Div(
-        dcc.Graph(figure=fig)
+        '''
+        An example of a climate dashboard
+        '''
+    ),
+    html.Img(
+        src=img2_url,
+        style={'width':'100%'}
+    ),
+    #divide up into two big divs, sidebar on left, graphs on right?
+    html.Div(
+        dcc.Graph(figure=fig),
     ),
     html.Div(
         dcc.Graph(figure=fig3),
@@ -244,12 +305,20 @@ app.layout = html.Div([
     ),
     html.Div(
         dcc.Graph(figure=fig2),
-        style={'width': '48%','display': 'inline-block'}
+        style={'width': '48%','display': 'inline-block','backgroundColor':'black'}
     ),
-    ]
+    html.Div(
+        dcc.Graph(figure=fig4),
+        style={'width': '48%','display': 'inline-block','backgroundColor':'black'}
+    ),
+    html.Div(
+        dcc.Graph(figure=fig5),
+        style={'width': '48%','display': 'inline-block','backgroundColor':'black', 'align':'right'}
+    ),
+    ], style={'backgroundColor':'black'}
 )
 if __name__ == '__main__':
-    app.run_server(debug=False, use_reloader=False)  # Turn off reloader if inside Jupyter nb or deploying in Heroku
+    app.run_server(debug=True, use_reloader=True)  # Turn off reloader if inside Jupyter nb or deploying in Heroku
 
 
 
